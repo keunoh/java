17c4c74d32f3d89714910cb81544e4516b200594
keunoh <rmsdhcjswo1@gmail.com>
2026-07-05 20:59:53 +0900

    hello commit

--- src/ref/Method2.java
+++ src/ref/Method2.java
@@ -3,7 +3,6 @@ package ref;
 public class Method2 {
     public static void main(String[] args) {
         Student student1 = createStudent( "학생1", 15, 90);
-
         Student student2 = createStudent( "학생2", 16, 80);
 
         printStudent(student1);
